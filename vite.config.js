8cc3f05827f277edb015bb33bbebf55a2ee73f07
mcsederberg <mcsederberg@gmail.com>
2025-01-10 15:08:47 -0600

    Update src

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: process.cwd(),
   plugins: [vue()],
+  resolve: {
+    alias: [
+      {find: "@", replacement: '/src'}
+    ],
+  },
 })
